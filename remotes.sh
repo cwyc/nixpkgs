50d78930fec3889eab35963dfb285cadd9b111c4
cwyc <16950437+cwyc@users.noreply.github.com>
2021-07-23 22:27:58 -0400

    message

--- remotes.sh
+++ remotes.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env bash
 git remote add grahamc https://github.com/grahamc/nixpkgs.git
+git remote add danielfullmer https://github.com/danielfullmer/nixpkgs.git
 git remote add upstream https://github.com/NixOS/nixpkgs.git
